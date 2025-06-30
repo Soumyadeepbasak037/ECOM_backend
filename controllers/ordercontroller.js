af3216ab1f1b6ee6b09d6e8e6348b93c4c9057dc
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-06-30 19:04:07 +0530

    Ordercontroller modified

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -4,7 +4,7 @@ exports.placeOrder = (req, res) => {
   const userId = req.user.id;
 
   // Get cart items
-  const cartItems = db
+  const cart_items = db
     .prepare(
       `
     SELECT c.product_id, c.quantity, p.price
@@ -15,7 +15,25 @@ exports.placeOrder = (req, res) => {
     )
     .all(userId);
 
-  if (cartItems.length === 0) {
+  if (cart_items.length === 0) {
     return res.status(400).json({ message: "Cart is empty." });
   }
+
+  const totalPrice = cartItems.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+
+  // Create oder
+  const order_stmt = db.prepare(`
+    INSERT INTO orders (user_id, total_price) VALUES (?, ?)
+  `);
+  const order_result = order_stmt.run(userId, totalPrice);
+  const orderId = order_result.lastInsertRowid;
+
+  // add items
+  const order_item_stmt = db.prepare(`
+    INSERT INTO order_items (order_id, product_id, quantity, price)
+    VALUES (?, ?, ?, ?)
+  `);
 };
