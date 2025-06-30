03a3aacca8dfda35b9d9c390d3252d685b0669da
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-07-01 00:58:47 +0530

    Changes made to ordercontroller to include add to cart features

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -1,5 +1,46 @@
 const db = require("../config/db");
 
+exports.add_to_card = (req, res) => {
+  const user_id = req.body.id;
+  const product_id = req.body.product_id;
+  const quantity = req.body.qty;
+
+  const product = db
+    .prepare(`SELECT * FROM products WHERE id = ?`)
+    .get(product_id);
+
+  if (!product) {
+    return res.status(400).json("invalid product id");
+  }
+
+  const existing = db
+    .prepare(
+      `
+    SELECT * FROM cart_items WHERE user_id = ? AND product_id = ?
+  `
+    )
+    .get(user_id, product_id);
+
+  if (existing) {
+    const stmt = db
+      .prepare(
+        `  UPDATE cart_items SET quantity = quantity + ?
+      WHERE user_id = ? AND product_id = ?`
+      )
+      .run(quantity, user_id, product_id);
+    res.json("Product already in cary quantity increased");
+  } else {
+    db.prepare(
+      `
+      INSERT INTO cart_items (user_id, product_id, quantity)
+      VALUES (?, ?, ?)
+    `
+    ).run(user_id, product_id, quantity);
+  }
+
+  res.json({ message: "Product added to cart" });
+};
+
 exports.placeOrder = (req, res) => {
   const userId = req.user.id;
 
@@ -36,4 +77,11 @@ exports.placeOrder = (req, res) => {
     INSERT INTO order_items (order_id, product_id, quantity, price)
     VALUES (?, ?, ?, ?)
   `);
+
+  const insertMany = db.transaction((items) => {
+    for (const item of items) {
+      order_item_stmt.run(orderId, item.product_id, item.quantity, item.price);
+    }
+  });
+  insertMany(cart_items);
 };
